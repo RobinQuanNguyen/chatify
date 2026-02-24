abc6a146a6b71d1226b13912a30837bdeeadb16e
RobinQuanNguyen <nguyenbaoquan2004@gmail.com>
2026-02-24 12:48:14 +0200

    update emails folder

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
 import { generateToken } from '../lib/utils.js';
+import { sendWelcomeEmail } from '../emails/emailHandlers.js';
+import { ENV } from '../lib/env.js';
 
 export const signup = async (req, res) => {
     const {fullName, email, password} = req.body;
@@ -45,12 +47,17 @@ export const signup = async (req, res) => {
 
             res.status(201).json({
                 _id: savedUser._id,
-                fullName: newUser.fullName,
-                email: newUser.email,
-                profilePic: newUser.profilePic,
+                fullName: savedUser.fullName,
+                email: savedUser.email,
+                profilePic: savedUser.profilePic,
             });
 
-            // todo: send a welcome email to the user
+            try {
+                await sendWelcomeEmail(savedUser.email, savedUser.fullName, ENV.CLIENT_URL);
+            } catch (error) {
+                console.log("Error sending welcome email", error.message);
+            }
+
         } else {
             res.status(400).json({message: "Invalid user data"});
         }
