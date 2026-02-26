527bd6eac36d63bdac3ee808cf55ff107f350dc3
RobinQuanNguyen <nguyenbaoquan2004@gmail.com>
2026-02-26 14:43:03 +0200

    Feat: sign in page created

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,6 +6,7 @@ export const useAuthStore = create((set) => ({
     authUser: null,
     isCheckingAuth: true,
     isSigningUp: false,
+    isLoggingIn: false,
 
     checkAuth: async () => {
         try {
@@ -34,4 +35,30 @@ export const useAuthStore = create((set) => ({
             set({isSigningUp: false})
         }
     },
+
+    logIn: async (data) => {
+        set({isLoggingIn: true});
+
+        try {
+            const res = await axiosInstance.post("/auth/login", data);
+            set({authUser: res.data})
+
+            // use react-hot-toast to show success message
+            toast.success("Logged in successfully!")
+        } catch (error) {
+            toast.error(error.response?.data?.message || "Error logging in")
+        } finally {
+            set({isLoggingIn: false})
+        }
+    },
+
+    logout: async () => {
+        try {
+            await axiosInstance.post("/auth/logout");
+            set({authUser: null})
+            toast.success("Logged out successfully!")
+        } catch (error) {
+            toast.error(error.response?.data?.message || "Error logging out")
+        }
+    }
 }))
