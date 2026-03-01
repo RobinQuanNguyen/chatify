bd70fec67280972f4307d44284ae165147a4545c
RobinQuanNguyen <nguyenbaoquan2004@gmail.com>
2026-03-01 13:23:34 +0200

    Feat: add socket connection to backend

---
+++ backend/src/lib/socket.js
@@ -0,0 +1,43 @@
+import { Server } from 'socket.io';
+import http from 'http';
+import express from 'express';
+import { ENV } from "./env.js"
+import { socketAuthMiddleware } from './middleware/socket.auth.middleware.js';
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: [ENV.CLIENT_URL],
+        credentials: true,
+    },
+});
+
+// Apply authentication middleware to all incoming socket connections
+io.use(socketAuthMiddleware);
+
+// Store online users and their socket IDs
+const userSocketMap = {}; //{userId:socketId}
+
+// Usw socket.io to listen for events from clients
+io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.user.username} (ID: ${socket.user._id})`);
+
+    // Update the online user list
+    const userId = socket.userId;
+    userSocketMap[userId] = socket.id;
+
+    // io.emit is used to send events to all connected clients, including the sender
+    io.emit("getOnlineUsers", Object.keys(userSocketMap)); // Send the list of online user IDs to all clients
+
+    socket.on("disconnect", () => {
+        console.log(`User disconnected: ${socket.user.username} (ID: ${socket.user._id})`);
+
+        // Remove the user from the online user list
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap)); // Update the list of online user IDs for all clients
+    });
+});
+
+export { io, app, server };
--- backend/src/server.js
+++ backend/src/server.js
@@ -9,8 +9,10 @@ import { connectDB } from './lib/db.js';
 
 import { ENV } from './lib/env.js';
 
+import { app, server } from './lib/socket.js'; // Import the Express app from the socket module, which also sets up Socket.IO and applies the authentication middleware
+//const app = express();
 
-const app = express();
+
 const __dirname = path.resolve();
 
 const PORT = ENV.RUN_PORT || 3000;
@@ -32,7 +34,7 @@ if (ENV.NODE_ENV === 'production') {
     })
 }
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectDB();
 });
