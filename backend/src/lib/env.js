04b7c53cdfd28c24f9f6a623522fce0a42c85047
RobinQuanNguyen <nguyenbaoquan2004@gmail.com>
2026-03-03 14:38:44 +0200

    Feat: account delete function for admin

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -15,6 +15,5 @@ export const ENV = {
     CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
     ARCJET_KEY: process.env.ARCJET_KEY,
     ARCJET_ENV: process.env.ARCJET_ENV,
-    ADMIN_ID: process.env.ADMIN_ID
 };
 
--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -2,6 +2,9 @@ import express from 'express';
 import { protectRoute, requireAdmin } from '../middleware/auth.middleware.js';
 import { arcjetProtection } from '../middleware/arcjet.middleware.js';
 
+import { deleteUserAndData } from '../controllers/admin.controller.js';
+
+
 const router = express.Router();
 
 router.use(arcjetProtection); // Apply Arcjet protection to all routes in this router
@@ -9,5 +12,7 @@ router.use(arcjetProtection); // Apply Arcjet protection to all routes in this r
 router.get("/check", protectRoute, requireAdmin, (req, res) => {
     res.status(200).json({ message: "User is an admin" });
 })
+
+router.delete("/:id", protectRoute, requireAdmin, deleteUserAndData);
 
 export default router;
