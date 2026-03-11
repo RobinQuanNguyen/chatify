d528702e3f846fd88d0108192e609c57335d7289
RobinQuanNguyen <nguyenbaoquan2004@gmail.com>
2026-03-11 14:15:56 +0200

    feat: setup for chatPageTest

--- frontend/src/tests/setup.js
+++ frontend/src/tests/setup.js
@@ -1,5 +1,18 @@
 import "@testing-library/jest-dom";
 
+if (!window.matchMedia) {
+	window.matchMedia = (query) => ({
+		matches: false,
+		media: query,
+		onchange: null,
+		addListener: () => {},
+		removeListener: () => {},
+		addEventListener: () => {},
+		removeEventListener: () => {},
+		dispatchEvent: () => false,
+	});
+}
+
 // import { afterEach } from "vitest";
 // import { cleanup } from "@testing-library/react";
 
